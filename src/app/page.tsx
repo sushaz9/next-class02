333ebf8d3808a8dd0856b9df04ea111faf687c2c
sushaz9 <susannahelisabeth1@gmail.com>
2023-11-20 15:47:05 +0000

    home page link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="max-h-[80%] flex items-center bg-green-200">
         <h2 className="text-lg pb-5 flex-col items-center">Hi! I'm Susie 👋🏼</h2>
         <p className="pb-2">Tailwind is tricky</p>
-        <p className="pb-3">whAt iS gOiNG on</p>
+        <p className="pb-4">whAt iS gOiNG on</p>
         <div className="flex min-h-screen flex-col items-center justify-between p-24">
           <img src="https://i.pinimg.com/736x/c1/c8/6d/c1c86dc8ebcde10d3f480eb59b37f104.jpg" />
         </div>
