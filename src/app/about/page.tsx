2642f6491001d06718fb445ad1008121e8a91103
sushaz9 <susannahelisabeth1@gmail.com>
2023-11-21 15:34:35 +0000

    dymanic attempt mostly notes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,52 @@
 import Link from "next/link";
 
+type roomsSearchQuery = {
+  sortBy: string;
+};
+
+type roomsType = {
+  name: string;
+  id: number;
+};
+
+let rooms: roomsType[] = [
+  {
+    name: "Vacancy",
+    id: 0,
+  },
+  {
+    name: "Sabotage",
+    id: 1,
+  },
+  {
+    name: "Wanted",
+    id: 2,
+  },
+  {
+    name: "Outbreak",
+    id: 3,
+  },
+  {
+    name: "Spellbound",
+    id: 4,
+  },
+  {
+    name: "Reclassified",
+    id: 5,
+  },
+];
+
 export default function Page() {
+  function comparePets(a: roomsType, b: roomsType) {
+    if (a.name < b.name) {
+      return -1;
+    } else if (a.name > b.name) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
   return (
     <div>
       <h1>This is a barebones tailwind web page. </h1>
